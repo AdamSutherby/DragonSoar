583af01090f5c86a67a6821a1292f67511440b88
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-02-25 14:23:16 -0700

    yep

---
+++ components/MainMenu.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function MainMenu() {
+  return (
+    <div>
+      <h1>Main Menu</h1>
+      <button>Start</button>
+      <button>Options</button>
+    </div>
+  );
+}
