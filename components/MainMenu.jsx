0bc7c2e80bd7036e39b2097eb90a09a6e5fa470e
AdamSutherby <123500035+AdamSutherby@users.noreply.github.com>
2024-02-25 15:37:33 -0700

    bunch of stuff

--- components/MainMenu.jsx
+++ components/MainMenu.jsx
@@ -1,11 +1,43 @@
 import react from 'react';
+import {
+     StyleSheet,
+    View,
+    Text,
+    TouchableOpacity
+ } from 'react-native';
 
-function MainMenu() {
-  return (
-    <div>
-      <h1>Main Menu</h1>
-      <button>Start</button>
-      <button>Options</button>
-    </div>
-  );
-}
+ function MainMenu() {
+    return (
+      <View style={[styles.menu, styles.background]}>
+        <Text style={styles.title}>DRAGONSOAR</Text>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Start</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Options</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    menu: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    background: {
+      backgroundColor: 'transparent',
+    },
+    title: {
+      fontSize: 50,
+      color: 'black',
+      marginBottom: 20,
+    },
+    button: {
+        margin: 10,
+        backgroundColor: 'transparent',
+    }
+  });
+  
+  export default MainMenu;
